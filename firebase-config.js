2421af26885d681cfbfe3978224b4e19990963e0
clawsbykait <claws.by.kait916@gmail.com>
2026-06-13 20:04:16 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const FIREBASE_CONFIG = {
   apiKey: "AIzaSyATsDuG6O8GiiUgMFpGUSp36H75_7v_wNM",
   authDomain: "claws-by-kait.firebaseapp.com",
   projectId: "claws-by-kait",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "229017104784",
   appId: "1:229017104784:web:436b20ab7c1d578a440315"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
